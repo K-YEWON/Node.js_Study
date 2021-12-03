c473e282de48622138f0819d4ea8b316e54641d4
yewon <yewon1914@gmail.com>
2021-12-03 17:31:12 +0900

    EventEmitter test

--- main.js
+++ main.js
@@ -1,8 +1,18 @@
-//간단한 웹서버 제작
+/*//간단한 웹서버 제작
 const http = require("http");
 
 let server = http.createServer(function (requrest, response) {
     response.end('<h1>Hello World!</h1>');
+});*
+
+server.listen(3000);*/
+
+const EventEmitter = require('events');
+
+const myEmitter = new EventEmitter();
+
+myEmitter.on('test', () => {
+    console.log('Success!');
 });
 
-server.listen(3000);
+myEmitter.emit('test');
