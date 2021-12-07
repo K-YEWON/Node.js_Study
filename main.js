d1274256b7233212871e59af30976fb8aff60cc3
yewon <yewon1914@gmail.com>
2021-12-07 13:10:29 +0900

    server & client http test

--- main.js
+++ main.js
@@ -1,18 +1,9 @@
-/*//간단한 웹서버 제작
-const http = require("http");
+/* 2021.12.07 server & client
+const http = require('http'); // http 코어모듈 사용
 
-let server = http.createServer(function (requrest, response) {
-    response.end('<h1>Hello World!</h1>');
-});*
-
-server.listen(3000);*/
-
-const EventEmitter = require('events');
-
-const myEmitter = new EventEmitter();
-
-myEmitter.on('test', () => {
-    console.log('Success!');
+let server = http.createServer(function(request, response) { //createServer(); 서버 역할을 하는 객체 하나 생성
+response.end('<h1>Hello world!</h1>'); //서버 객체가 클라이언트 요청을 받을 시 응답할 내용 설정
 });
 
-myEmitter.emit('test');
+server.listen(3000); //서버 객체가 외부 요청 받아들임 3000:포트번호
+*/
